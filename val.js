a2d0fbac38c4a75e3f63f268c3a8fcc3727e24ca
Labonno Khan Bonna <bonnalabonnokhan2708@gmail.com>
2023-11-01 20:20:09 +0600

    my second commit

---
+++ val.js
@@ -0,0 +1,14 @@
+const numher = '23';
+if (typeof number == 'number'){
+    console.log('value is a number')
+}
+else{
+    console.log('value is not a number')
+}
+
+const numbers = [1,2,3,8,5];
+const student = {name : 'Rafsan' , job : 'khoya'}
+
+
+console.log(typeof numbers);
+console.log(typeof student);
